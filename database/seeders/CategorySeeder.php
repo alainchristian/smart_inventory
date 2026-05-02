1848c9d90f44d68900cf3de57bf168ed520340b7
Christian <Christian@ASYV-ADMIN-PC13.lnfhs.asyv.org>
2026-05-02 16:16:03 +0200

    categories2

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,25 +10,26 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['name' => 'Footwear',       'description' => 'All footwear products'],
-            ['name' => 'Sneakers',       'description' => null],
-            ['name' => 'Dress Shoes',    'description' => null],
-            ['name' => 'Sandals',        'description' => null],
-            ['name' => 'School Shoes',   'description' => null],
-            ['name' => 'Sports Shoes',   'description' => null],
-            ['name' => "Ladies' Heels",  'description' => null],
-            ['name' => 'Boots',          'description' => null],
-            ['name' => 'Slippers',       'description' => null],
-            ['name' => 'Accessories',    'description' => 'Shoe & fashion accessories'],
-            ['name' => 'Socks',          'description' => null],
-            ['name' => 'Belts',          'description' => null],
-            ['name' => 'Bags',           'description' => null],
-            ['name' => 'Shoe Care',      'description' => null],
+            ['name' => 'Footwear',       'code' => 'FOOT',     'description' => 'All footwear products'],
+            ['name' => 'Sneakers',       'code' => 'FOOT-SNK', 'description' => null],
+            ['name' => 'Dress Shoes',    'code' => 'FOOT-DRS', 'description' => null],
+            ['name' => 'Sandals',        'code' => 'FOOT-SND', 'description' => null],
+            ['name' => 'School Shoes',   'code' => 'FOOT-SCH', 'description' => null],
+            ['name' => 'Sports Shoes',   'code' => 'FOOT-SPT', 'description' => null],
+            ['name' => "Ladies' Heels",  'code' => 'FOOT-LDH', 'description' => null],
+            ['name' => 'Boots',          'code' => 'FOOT-BOT', 'description' => null],
+            ['name' => 'Slippers',       'code' => 'FOOT-SLP', 'description' => null],
+            ['name' => 'Accessories',    'code' => 'ACC',      'description' => 'Shoe & fashion accessories'],
+            ['name' => 'Socks',          'code' => 'ACC-SOC',  'description' => null],
+            ['name' => 'Belts',          'code' => 'ACC-BLT',  'description' => null],
+            ['name' => 'Bags',           'code' => 'ACC-BAG',  'description' => null],
+            ['name' => 'Shoe Care',      'code' => 'ACC-CRE',  'description' => null],
         ];
 
         foreach ($categories as $cat) {
             DB::table('categories')->insertOrIgnore([
                 'name'        => $cat['name'],
+                'code'        => $cat['code'],
                 'description' => $cat['description'],
                 'is_active'   => true,
                 'created_at'  => now(),
